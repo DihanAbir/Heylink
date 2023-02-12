8e6865479fa21fbaae019048f8d8230fb3a3b367
shahariarraghib <sr.raghib@gmail.com>
2023-02-13 03:58:44 +0600

    intregation added

--- src/components/Modals/CustomizeLinkModals/ImageUploadModal.js
+++ src/components/Modals/CustomizeLinkModals/ImageUploadModal.js
@@ -1,91 +1,119 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import close from '../../../assets/icons/link-customize-icons/close.svg'
-import lock from '../../../assets/icons/link-customize-icons/pro-lock.svg'
-import FastLinkProModal from './FastLinkProModal';
+import React, { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import close from "../../../assets/icons/link-customize-icons/close.svg";
+import lock from "../../../assets/icons/link-customize-icons/pro-lock.svg";
+import FastLinkProModal from "./FastLinkProModal";
 
-const ImageUploadModal = ({ closeModal, imageData }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm()
-    const [openFastLinkModal, setOpenFastLinkModal] = useState(false)
-    const key = '0ea873c4c7476f789bc1d4c4e7b5545b';
+const ImageUploadModal = ({ closeModal, url }) => {
+  const imageID = url?._id;
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [openFastLinkModal, setOpenFastLinkModal] = useState(false);
 
-    const ImageUpload = (data) => {
-        const image = data;
-        const formData = new FormData()
-        formData.append('image', image)
-        const uri = `https://api.imgbb.com/1/upload?key=${key}`
-        fetch(uri, {
-            method: 'POST',
-            body: formData
-        })
-            .then(res => res.json())
-            .then(imgData => {
-                imageData(imgData)
-                closeModal(false)
-            })
-    }
+  const ImageUpload = (data) => {
+    const formData = new FormData();
+    formData.append("file", data.image[0]);
 
-    let dropdownRef = useRef();
-    useEffect(() => {
-        let handler = (e) => {
-            if (!dropdownRef.current.contains(e.target)) {
-                closeModal(false);
-            }
-        };
-        document.addEventListener("mousedown", handler);
-        return () => {
-            document.removeEventListener("mousedown", handler);
-        }
-    });
+    const url = `http://localhost:8000/app/v1/links/common/${imageID}`;
+    fetch(url, {
+      method: "PATCH",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
-    const closeModal2 = () => {
-        setOpenFastLinkModal(false)
-    }
-    return (
-        <div ref={dropdownRef}
-            className="cursor-pointer absolute border -bottom-8 z-50 mt-2 rounded-md bg-gray-50 shadow">
-            <div className='p-3'>
-                <div className='w-full flex justify-end'>
-                    <img onClick={() => closeModal(false)} className='w-3' src={close} alt="" />
-                </div>
-                <form onChange={handleSubmit(ImageUpload)}>
-                    <div className='grid grid-cols-2 gap-6 mt-3'>
-                        <div onClick={() => closeModal(false)} className="relative w-24 h-24 flex justify-center items-center bg-gray-200 hover:bg-blue-600 rounded-md">
-                            <label className="cursor-pointer flex justify-center items-center">
-                                <div className=" relative flex cursor-pointer items-center justify-center">
-                                    <span className='p-2 hover:text-white text-center text-gray-600 text-sm'>Upload Your own Image</span>
-                                    <input {...register('image', { required: 'image is Required' })}
-                                        type="file" name='image' className="w-6 absolute opacity-0 cursor-pointer" />
-                                </div>
-                            </label>
-                        </div>
-                        <div onClick={() => setOpenFastLinkModal(!openFastLinkModal)}
-                            className="relative w-24 h-24 flex justify-center items-center bg-gray-200 rounded-md">
-                            <label className="cursor-pointer flex justify-center items-center">
-                                <div className=" relative flex cursor-pointer items-center justify-center">
-                                    <span className='p-2 hover:text-white text-center text-gray-600 text-sm'>Choose from 700+ icons</span>
-                                    <input type="file" name='image' className="w-6 absolute opacity-0 cursor-pointer" />
-                                </div>
-                            </label>
-                            <div className='absolute flex justify-center items-center w-24 h-24'>
-                                <div className='bg-white h-8 w-8 p-1 rounded-full'>
-                                    <img className='w-full' src={lock} alt="" />
-                                </div>
-                            </div>
-                            <div className='absolute -top-4 right-6 flex justify-center items-center w-10 h-4 rounded-3xl bg-[#F06957]'>
-                                <img className='w-7' src={lock} alt="" />
-                            </div>
-                        </div>
-                    </div>
-                </form>
+  let dropdownRef = useRef();
+  useEffect(() => {
+    let handler = (e) => {
+      if (!dropdownRef.current.contains(e.target)) {
+        closeModal(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
-                {
-                    openFastLinkModal && <FastLinkProModal closeModal2={closeModal2} />
-                }
-            </div>
+  const closeModal2 = () => {
+    setOpenFastLinkModal(false);
+  };
+  return (
+    <div
+      ref={dropdownRef}
+      className="cursor-pointer absolute border -bottom-8 z-50 mt-2 rounded-md bg-gray-50 shadow"
+    >
+      <div className="p-3">
+        <div className="w-full flex justify-end">
+          <img
+            onClick={() => closeModal(false)}
+            className="w-3"
+            src={close}
+            alt=""
+          />
         </div>
-    );
+        <form
+          onChange={handleSubmit(ImageUpload)}
+          encType="multipart/form-data"
+        >
+          <div className="grid grid-cols-2 gap-6 mt-3">
+            <div
+              //   onClick={() => closeModal(false)}
+              className="relative w-24 h-24 flex justify-center items-center bg-gray-200 hover:bg-blue-600 rounded-md"
+            >
+              <label className="cursor-pointer flex justify-center items-center">
+                <div className=" relative flex cursor-pointer items-center justify-center">
+                  <span className="p-2 hover:text-white text-center text-gray-600 text-sm">
+                    Upload Your own Image
+                  </span>
+                  <input
+                    {...register("image", { required: "image is Required" })}
+                    type="file"
+                    name="image"
+                    className="w-6 absolute opacity-0 cursor-pointer"
+                  />
+                </div>
+              </label>
+            </div>
+            <div
+              onClick={() => setOpenFastLinkModal(!openFastLinkModal)}
+              className="relative w-24 h-24 flex justify-center items-center bg-gray-200 rounded-md"
+            >
+              <label className="cursor-pointer flex justify-center items-center">
+                <div className=" relative flex cursor-pointer items-center justify-center">
+                  <span className="p-2 hover:text-white text-center text-gray-600 text-sm">
+                    Choose from 700+ icons
+                  </span>
+                  <input
+                    type="file"
+                    name="image"
+                    className="w-6 absolute opacity-0 cursor-pointer"
+                  />
+                </div>
+              </label>
+              <div className="absolute flex justify-center items-center w-24 h-24">
+                <div className="bg-white h-8 w-8 p-1 rounded-full">
+                  <img className="w-full" src={lock} alt="" />
+                </div>
+              </div>
+              <div className="absolute -top-4 right-6 flex justify-center items-center w-10 h-4 rounded-3xl bg-[#F06957]">
+                <img className="w-7" src={lock} alt="" />
+              </div>
+            </div>
+          </div>
+        </form>
+
+        {openFastLinkModal && <FastLinkProModal closeModal2={closeModal2} />}
+      </div>
+    </div>
+  );
 };
 
 export default ImageUploadModal;
